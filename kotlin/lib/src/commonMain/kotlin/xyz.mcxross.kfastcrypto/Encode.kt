0717ad401bf8a44887b296a0ffb165fb4bea86ca
@a <28899947+astinz@users.noreply.github.com>
2023-07-07 16:06:35 +0300

    [kotlin] call native...

--- kotlin/lib/src/commonMain/kotlin/xyz.mcxross.kfastcrypto/Encode.kt
+++ kotlin/lib/src/commonMain/kotlin/xyz.mcxross.kfastcrypto/Encode.kt
@@ -1,8 +1,8 @@
 package xyz.mcxross.kfastcrypto
 
 object Encode {
-  fun base64ToHex(bytes: String): String = ""
+  fun base64ToHex(bytes: String): String = xyz.mcxross.kfastcrypto.base64ToHex(bytes)
 
-  fun hexToBase64(bytes: String): String = ""
+  fun hexToBase64(bytes: String): String = xyz.mcxross.kfastcrypto.hexToBase64(bytes)
 
 }
