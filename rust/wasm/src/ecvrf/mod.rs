40b9b6ffdaae9d5cd0383517613fb5ed709fe74b
@a <28899947+astinz@users.noreply.github.com>
2023-07-21 17:54:30 +0300

    [wasm] add kfastcrypto ecvrf bindings...

--- rust/wasm/src/ecvrf/mod.rs
+++ rust/wasm/src/ecvrf/mod.rs
@@ -1,20 +1,35 @@
+use kfc_core::ecvrf;
 use wasm_bindgen::prelude::*;
-use kfc_core::ecvrf::keygen;
 
 #[wasm_bindgen]
-extern "C" {
-    fn alert(s: &str);
+pub fn keygen() -> Result<String, JsValue> {
+    let keypair = match ecvrf::keygen() {
+        Ok(pair) => pair,
+        Err(e) => return Err(JsValue::from_str(&e.to_string())),
+    };
+    Ok(format!("{}", keypair))
 }
 
 #[wasm_bindgen]
-pub fn greet() {
-    let result = keygen();
-    match result {
-        Ok(res) => {
-            alert(format!("{}", res).as_str());
-        }
-        Err(e) => {
-            alert(e.to_string().as_str());
-        }
+pub fn prove(input: &str, secret_key: &str) -> Result<String, JsValue> {
+    let proof = match ecvrf::prove(input.to_string(), secret_key.to_string()) {
+        Ok(proof) => proof,
+        Err(e) => return Err(JsValue::from_str(&e.to_string())),
+    };
+    Ok(format!("{}", proof))
+}
+
+#[wasm_bindgen]
+pub fn verify(output: &str, proof: &str, input: &str, pub_key: &str) -> Result<bool, JsValue> {
+    let result = ecvrf::verify(
+        output.to_string(),
+        proof.to_string(),
+        input.to_string(),
+        pub_key.to_string(),
+    );
+    if result.is_ok() {
+        Ok(true)
+    } else {
+        Err(JsValue::from_str("Verification failed"))
     }
 }
